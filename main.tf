2dc754f3e2f402cd67c1ec61913cf90d9884596d
Naveen-manokaran <naveen.manokaran@bootlabstech.com>
2022-08-30 15:05:55 +0530

    adding the tags

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "google_redis_instance" "gcp_redis" {
-  name                    = var.name
-  memory_size_gb          = var.memory_size_gb
-  authorized_network      = var.authorized_network
-  redis_configs           = var.redis_configs
-  redis_version           = var.redis_version
+  name               = var.name
+  memory_size_gb     = var.memory_size_gb
+  authorized_network = var.authorized_network
+  redis_configs      = var.redis_configs
+  redis_version      = var.redis_version
   #reserved_ip_range       = var.reserved_ip_range
   tier                    = var.tier
   region                  = var.region
