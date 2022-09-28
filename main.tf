334a32f7426b62e6a033c49b54619d0bf9ac3e68
Silpashree <silpashree@bootlabstech.com>
2022-09-28 22:32:15 +0530

    added api

--- main.tf
+++ main.tf
@@ -2,7 +2,12 @@ data "google_compute_network" "redis-network" {
   name    = var.name_reserved_ip_range
   project = var.host_project_id
 }
+resource "google_project_service" "redisapi" {
+  project = var.project_id
+  service = "redis.googleapis.com"
+}
 resource "google_redis_instance" "gcp_redis" {
+  depends_on              = [google_project_service.redisapi]
   name                    = var.name
   memory_size_gb          = var.memory_size_gb
   authorized_network      = var.authorized_network
