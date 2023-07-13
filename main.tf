380d80ee312961aaa0baeda2cb3b4b36cd963720
Silpashree Acharya <silpashree@bootlabstech.com>
2023-07-13 12:37:19 +0530

    reference auth network

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "google_redis_instance" "gcp_redis" {
   depends_on              = [google_project_service.redisapi]    
   name                    = var.redis_name
   memory_size_gb          = var.memory_size_gb 
-  authorized_network      = var.authorized_network
+  authorized_network      = data.google_compute_network.redis-network.id
   redis_configs           = var.redis_configs
   redis_version           = var.redis_version
   tier                    = var.tier
