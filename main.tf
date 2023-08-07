a92023cf9523e9d21f184f3c5dfaee9ff8110f3b
Silpashree Acharya <silpashree@bootlabstech.com>
2023-08-07 20:25:16 +0530

    lifecycle block

--- main.tf
+++ main.tf
@@ -7,9 +7,9 @@ resource "google_project_service" "redisapi" {
   service = "redis.googleapis.com"
 }
 resource "google_redis_instance" "gcp_redis" {
-  depends_on              = [google_project_service.redisapi]    
+  depends_on              = [google_project_service.redisapi]
   name                    = var.redis_name
-  memory_size_gb          = var.memory_size_gb 
+  memory_size_gb          = var.memory_size_gb
   authorized_network      = data.google_compute_network.redis-network.id
   redis_configs           = var.redis_configs
   redis_version           = var.redis_version
@@ -20,4 +20,7 @@ resource "google_redis_instance" "gcp_redis" {
   transit_encryption_mode = var.transit_encryption_mode
   connect_mode            = var.connect_mode
   reserved_ip_range       = var.name_reserved_ip_range
+  lifecycle {
+    ignore_changes = [labels]
+  }
 }
